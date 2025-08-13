e70036a5f4308b2b92396a583efc40335093652b
Nowroz Islam <116680399+nowroz@users.noreply.github.com>
2025-08-13 10:25:44 +0600

    Install kanagawa and gruvbox colorscheme plugins
    Set kanagawa as the default colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  { "https://github.com/rebelot/kanagawa.nvim" },
+
+  { "ellisonleao/gruvbox.nvim" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+    },
+  },
+}
