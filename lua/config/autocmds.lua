f5247150ed186a1861c09d07065b0f1e8ba2e57b
Nowroz Islam <116680399+nowroz@users.noreply.github.com>
2025-08-13 14:42:07 +0600

    Disable auto-commenting newline when o is pressed

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,10 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Disable auto commenting newline when pressing o
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function()
+    vim.opt.formatoptions:remove("o")
+  end,
+})
