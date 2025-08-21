8d5b50077e84809f0b3676b8852b71ba70e342e5
Nowroz Islam <116680399+nowroz@users.noreply.github.com>
2025-08-21 07:36:19 +0600

    Install new colorschemes and update plugins

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,6 +4,15 @@ return {
   { "ellisonleao/gruvbox.nvim" },
 
   {
+    "rose-pine/neovim",
+    name = "rose-pine",
+  },
+
+  {
+    "vague2k/vague.nvim",
+  },
+
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "kanagawa",
