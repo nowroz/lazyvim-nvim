2f151205780e6259100493ea19e95329ad05a53b
Nowroz Islam <116680399+nowroz@users.noreply.github.com>
2025-08-30 01:20:31 +0600

    Delete keymaps to reduce conflict

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,13 +9,3 @@ keymap.set("n", "x", '"_x')
 -- Increment/decrement
 keymap.set("n", "+", "<C-a>")
 keymap.set("n", "-", "<C-x>")
-
--- Split window
-keymap.set("n", "ss", ":split<Return>", opts)
-keymap.set("n", "sv", ":vsplit<Return>", opts)
-
--- Move window focus
-keymap.set("n", "sh", "<C-w>h")
-keymap.set("n", "sk", "<C-w>k")
-keymap.set("n", "sj", "<C-w>j")
-keymap.set("n", "sl", "<C-w>l")
